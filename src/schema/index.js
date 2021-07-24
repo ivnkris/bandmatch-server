3af5a97aebe386491d0203751e255c537f281ffa
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-07-24 12:19:22 +0100

    added graphQL schemas

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,5 +1,49 @@
 const { gql } = require("apollo-server");
 
-const typeDefs = gql``;
+const typeDefs = gql`
+  type User {
+    id: ID!
+    type: String!
+    isPremium: Boolean!
+    postcode: String!
+    genre: String
+    experienceLevel: String
+    numberOfMembers: Int
+    instrument: [String]
+    photoUrl: String
+    bandName: String
+    firstName: String
+    lastName: String
+    description: String
+    soundCloudUrl: String
+    lookingFor: [String]
+    openToMembers: Boolean
+    openToCollaboration: Boolean
+    openToJoiningBand: Boolean
+    venueName: String
+    websiteUrl: String
+    bandId: ID
+    favourites: [ID]
+    gigs: [ID]
+  }
+
+  type Gig {
+    id: ID!
+    title: String!
+    description: String!
+    genre: String!
+    fee: Int!
+    postcode: String!
+    websiteUrl: String
+    dateTime: String!
+  }
+
+  type Query {
+    user(id: ID!): User
+    users(sortBy: String, top: Int, filters: [String]): [User]
+    gigs(sortBy: String, top: Int, filters: [String]): [Gig]
+    gig(id: ID!): Gig
+  }
+`;
 
 module.exports = typeDefs;
