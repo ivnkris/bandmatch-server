4a7d33ea4fa325ef13ee8094d461892b2b76631e
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-08-09 19:09:13 +0100

    fixed assemble and collaborate resolvers

--- src/resolvers/assemble.js
+++ src/resolvers/assemble.js
@@ -1,8 +1,14 @@
 const { MusicianUser, Band } = require("../models");
 
 const assemble = async (_, args) => {
-  const bands = await Band.find({});
-  const musicians = await MusicianUser.find({});
+  const bands = await Band.find({})
+    .populate("genre")
+    .populate("instruments")
+    .populate("lookingFor");
+  const musicians = await MusicianUser.find({})
+    .populate("genre")
+    .populate("instruments")
+    .populate("lookingFor");
 
   return { musicians, bands };
 };
--- src/resolvers/collaborate.js
+++ src/resolvers/collaborate.js
@@ -1,8 +1,14 @@
 const { MusicianUser, Band } = require("../models");
 
 const collaborate = async (_, args) => {
-  const bands = await Band.find({ openToCollaboration: true });
-  const musicians = await MusicianUser.find({ openToCollaboration: true });
+  const bands = await Band.find({ openToCollaboration: true })
+    .populate("genre")
+    .populate("instruments")
+    .populate("lookingFor");
+  const musicians = await MusicianUser.find({ openToCollaboration: true })
+    .populate("genre")
+    .populate("instruments")
+    .populate("lookingFor");
 
   return { musicians, bands };
 };
