e88175232c37022d564f710e91e7a0bda4f2d99b
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-08-12 19:09:04 +0100

    added collaborate resolver with filters

--- src/resolvers/collaborate.js
+++ src/resolvers/collaborate.js
@@ -1,16 +1,83 @@
+const mongoose = require("mongoose");
+
 const { MusicianUser, Band } = require("../models");
 
-const collaborate = async (_, args) => {
-  const bands = await Band.find({ openToCollaboration: true })
-    .populate("genre")
-    .populate("instruments")
-    .populate("lookingFor");
-  const musicians = await MusicianUser.find({ openToCollaboration: true })
-    .populate("genre")
-    .populate("instruments")
-    .populate("lookingFor");
+const constructFilters = (filters) => {
+	const array = Object.entries(filters);
 
-  return { musicians, bands };
+	const filterObject = array.reduce((acc, each) => {
+		if (each[0] === "experienceLevel") {
+			return {
+				...acc,
+				[each[0]]: { $in: each[1] },
+			};
+		} else if (each[0] !== "userType") {
+			return {
+				...acc,
+				[each[0]]: { $in: each[1] },
+			};
+		} else {
+			return acc;
+		}
+	}, {});
+
+	return filterObject;
+};
+
+const getBands = async (filters) => {
+	const bands = await Band.find(
+		constructFilters(filters || {}) && { openToCollaboration: true }
+	)
+		.populate("genre")
+		.populate("instruments")
+		.populate("lookingFor");
+
+	return bands;
+};
+
+const getMusicians = async (filters) => {
+	const musicians = await MusicianUser.find(
+		constructFilters(filters || {}) && { openToCollaboration: true }
+	)
+		.populate("genre")
+		.populate("instruments")
+		.populate("lookingFor");
+
+	return musicians;
+};
+
+const collaborate = async (_, { filters }) => {
+	if (filters) {
+		let cleansedFilters = {};
+		Object.keys(filters).forEach((filterKey) => {
+			if (filters[filterKey].length) {
+				cleansedFilters[filterKey] = filters[filterKey];
+			}
+		});
+
+		if (!cleansedFilters.userType || cleansedFilters.userType.length === 2) {
+			const bands = await getBands(cleansedFilters);
+			const musicians = await getMusicians(cleansedFilters);
+
+			return { musicians, bands };
+		}
+
+		if (cleansedFilters.userType[0] === "band") {
+			const bands = await getBands(cleansedFilters);
+			return { bands, musicians: [] };
+		}
+
+		if (cleansedFilters.userType[0] === "musician") {
+			const musicians = await getMusicians(cleansedFilters);
+			return { musicians, bands: [] };
+		}
+		return;
+	} else {
+		const bands = await getBands(cleansedFilters);
+		const musicians = await getMusicians(cleansedFilters);
+
+		return { musicians, bands };
+	}
 };
 
 module.exports = collaborate;
