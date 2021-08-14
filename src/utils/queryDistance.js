39f245689c8cce66685e7e18b9ce41769299f60b
Krisztian Ivan <krisztian.ivan@gmail.com>
2021-08-14 13:00:33 +0100

    tested queryDistance with different inputs and refactored to return relevant results

--- src/utils/queryDistance.js
+++ src/utils/queryDistance.js
@@ -3,16 +3,21 @@ const axios = require("axios");
 const queryDistance = async function (origins, destinations) {
 	const API_KEY = "AIzaSyCBKF4t5DY9nczpeG3uxAAHxU1NcNlynh4";
 
-	let formattedDestinations = [];
-	destinations.forEach((destination) => {
-		formattedDestinations.push(destination + "%7C");
+	const formattedDestinations = destinations.map((destination) => {
+		destination = destination.replace(/\s/g, "+");
+		destination = destination + "+UK" + "%7C";
+		return destination;
 	});
 
-	formattedDestinations.map((destination) => destination.replace(/\s/g, "+"));
-
-	const formattedOrigin = origins.map((origin) => origin.replace(/\s/g, "+"));
+	const formattedOrigin = origins.map((origin) => {
+		origin = origin.replace(/\s/g, "+");
+		origin = origin + "+UK";
+		return origin;
+	});
 
 	const url = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${formattedOrigin}&destinations=${formattedDestinations}&mode=driving&language=en-En&key=${API_KEY}`;
+
+	console.log(url);
 
 	const headers = {
 		"Content-Type": "text/plain",
