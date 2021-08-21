b037ae590aaf31b3286c97782a56bbc5e09b22bf
Dominika <domi_nika@hotmail.co.uk>
2021-08-21 13:42:21 +0100

    got rid of unused code

--- src/resolvers/checkIfMusicianExists.js
+++ src/resolvers/checkIfMusicianExists.js
@@ -2,13 +2,6 @@ const { MusicianUser } = require("../models");
 
 const checkIfMusicianExists = async (_, { input }) => {
   const { musicians } = input;
-  console.log(musicians);
-
-  // const musicianEmail = musicians[0];
-
-  // const result = await MusicianUser.findOne({ email: musicianEmail });
-
-  // console.log(result);
 
   const musicianValidationArray = musicians.map(async (musicianEmail) => {
     const user = await MusicianUser.findOne({ email: musicianEmail });
