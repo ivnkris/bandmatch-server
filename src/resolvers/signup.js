ae7327df309e30f2d3db974238c100460ac3205e
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-08-21 11:40:50 +0100

    sign up resolver final refactor

--- src/resolvers/signup.js
+++ src/resolvers/signup.js
@@ -3,16 +3,10 @@ const { MusicianUser, Genre, Instrument } = require("../models");
 const { tokenise } = require("../utils/tokenise");
 
 const signUp = async (_, { input }) => {
-  const newUser = await MusicianUser.create(input);
-
-  const user = await MusicianUser.findOne({ _id: newUser._id })
-    .populate("genre")
-    .populate("instruments")
-    .populate("lookingFor");
+  const user = await MusicianUser.create(input);
 
   const token = tokenise({ id: user.id, email: user.email });
 
-  console.log("joe", user);
   return { token, user };
 };
 
