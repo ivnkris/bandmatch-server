f87130e7544bd7956c4aff17a69155e9c25d3266
Dominika <domi_nika@hotmail.co.uk>
2021-08-23 07:16:48 +0100

    removed one console.log

--- src/resolvers/createGig.js
+++ src/resolvers/createGig.js
@@ -3,7 +3,6 @@ const { Gig } = require("../models");
 const createGig = async (_, { input }) => {
   console.log("creating gig", input);
   const gig = await Gig.create({ ...input });
-  console.log("result", gigCreationResult);
   return gig;
 };
 
