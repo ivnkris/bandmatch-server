35690cdbe61ecaba859f1d5e8513f594f691fec7
Dominika <domi_nika@hotmail.co.uk>
2021-08-25 21:44:49 +0100

    accounts for bands

--- src/resolvers/createMessage.js
+++ src/resolvers/createMessage.js
@@ -1,38 +1,66 @@
 const { Message, Conversation } = require("../models");
+const {
+  createConversationForMusicians,
+  createConversationForBandSender,
+  createConversationForBandRecipient,
+} = require("../utils/createConversations");
+const validateBandUser = require("../utils/validateBandUser");
 
 const createMessage = async (_, { input }) => {
-	const senderId = input.senderId;
-	const text = input.text;
-	const recipientId = input.recipientId;
+  const senderId = input.senderId;
+  const text = input.text;
+  const recipientId = input.recipientId;
 
-	const message = await Message.create({ senderId, text });
-	const conversations = await Conversation.find({});
+  const message = await Message.create({ senderId, text });
+  const conversations = await Conversation.find({});
 
-	if (conversations) {
-		const foundConversation = conversations.find(
-			(conversation) =>
-				conversation.participants.includes(senderId) &&
-				conversation.participants.includes(recipientId)
-		);
+  const isSenderBand = await validateBandUser(senderId);
+  const isRecipientBand = await validateBandUser(recipientId);
 
-		if (foundConversation) {
-			await Conversation.findByIdAndUpdate(foundConversation._id, {
-				$push: { messages: message._id },
-			});
-		} else {
-			await Conversation.create({
-				participants: [senderId, recipientId],
-				messages: [message._id],
-			});
-		}
-	} else {
-		await Conversation.create({
-			participants: [senderId, recipientId],
-			messages: [message._id],
-		});
-	}
+  let foundConversation = null;
+  // find if conversation between the two participants already exists
+  if (conversations) {
+    // find if conversation already exists if one of the participants is a band member
+    // logic for if the sender is a band
+    if (isSenderBand) {
+      const foundConversation = conversations.find(
+        (conversation) =>
+          conversation.bands.includes(senderId) &&
+          conversation.musicians.includes(recipientId)
+      );
+    } else if (isRecipientBand) {
+      // logic for if the recipient is a band
+      const foundConversation = conversations.find(
+        (conversation) =>
+          conversation.musicians.includes(senderId) &&
+          conversation.bands.includes(recipientId)
+      );
+    } else {
+      const foundConversation = conversations.find(
+        (conversation) =>
+          conversation.musicians.includes(senderId) &&
+          conversation.musicians.includes(recipientId)
+      );
+    }
+  }
 
-	return message;
+  // if conversation already exists update it with the new messaeg
+  if (foundConversation) {
+    await Conversation.findByIdAndUpdate(foundConversation._id, {
+      $push: { messages: message._id },
+    });
+  } else {
+    // if a conversation between the two participants doesn't exist, create one
+    if (isSenderBand) {
+      createConversationForBandSender(message._id, senderId, recipientId);
+    } else if (isRecipientBand) {
+      createConversationForBandRecipient(message._id, senderId, recipientId);
+    } else {
+      createConversationForMusicians(message._id, senderId, recipientId);
+    }
+  }
+
+  return message;
 };
 
 module.exports = createMessage;
