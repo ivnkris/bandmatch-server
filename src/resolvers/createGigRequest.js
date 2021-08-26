070d5113aeff2c60746ac0d89b18963927eef5e5
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-08-26 21:10:36 +0100

    gig request resolverupdate

--- src/resolvers/createGigRequest.js
+++ src/resolvers/createGigRequest.js
@@ -2,25 +2,14 @@ const { Gig } = require("../models");
 
 const createGigRequest = async (_, { input }) => {
   const performersObject = {};
-  // let foundGig;
 
-  // if (input.performer.musician) {
-  //   performersObject.musician = input.performer.musician;
-  //   foundGig = await Gig.find({
-  //     _id: input.id,
-  //     performers: { $in: { musician: input.performer.musician } },
-  //   });
-  // }
+  if (input.performer.musician) {
+    performersObject.musician = input.performer.musician;
+  }
 
-  // if (input.performer.band) {
-  //   performersObject.band = input.performer.band;
-  //   foundGig = await Gig.find({
-  //     _id: input.id,
-  //     $in: { performers: { band: input.performer.band } },
-  //   });
-  // }
-
-  // console.log(foundGig);
+  if (input.performer.band) {
+    performersObject.band = input.performer.band;
+  }
 
   const gig = await Gig.findOneAndUpdate(
     { _id: input.id },
