47d606125374fac0417b6905a1a461cf30161f52
Natasha Mann <natasha.s.mann@gmail.com>
2021-10-10 21:05:00 +0100

    update

--- src/server.js
+++ src/server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("build"));
 }
 
-app.get("/", function (req, res) {
+app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
