e006c0ad271ab723152a413d8310fe9ee932895e
Natasha Mann <natasha.s.mann@gmail.com>
2021-10-16 07:40:21 +0100

    update

--- src/server.js
+++ src/server.js
@@ -27,8 +27,6 @@ startServer();
 
 app.use(cors());
 
-app.use(express.static(path.join(__dirname, "build")));
-
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
